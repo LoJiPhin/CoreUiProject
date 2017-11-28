4084c80c673e25c8364b4aa474d2e26dee7433ae
kvardhineedi <kvardhineedi@tesla.com>
2017-11-28 21:02:16 +0530

    Add project files.

---
+++ CoreUiProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreUiProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
